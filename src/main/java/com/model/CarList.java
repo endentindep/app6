16ef0c65955736c926411faf588595b8868e7637
SkyBit <nikita1-2013@mail.ru>
2023-07-01 11:16:05 +0400

    war file fix

--- src/main/java/com/model/CarList.java
+++ src/main/java/com/model/CarList.java
@@ -1,10 +1,15 @@
 package com.model;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class CarList {
-	private static List<Car> carList = new ArrayList<>();
+	private static List<Car> carList = Arrays.asList(
+			new Car("model1", 1, 1),
+			new Car("model2", 2, 2),
+			new Car("model3", 3, 3),
+			new Car("model4", 4, 4),
+			new Car("model5", 5, 5));
 
 	public static void addCar(Car car) {
 		carList.add(car);
